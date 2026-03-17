e4b5db991556e5c67a046b1755f03b56b4251176
Khush Jain <khushjain8419@gmail.com>
2026-03-17 18:49:36 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCMDfMPuNxGirRIroAbCUSElsP6JsJ8PEM",
+  authDomain: "auctionmales.firebaseapp.com",
+  projectId: "auctionmales",
+  storageBucket: "auctionmales.firebasestorage.app",
+  messagingSenderId: "33238139903",
+  appId: "1:33238139903:web:c62d92f5daa4d9a21e5dca",
+  measurementId: "G-ZG457T4W9H"
+};
