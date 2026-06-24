7de335868fee15495f2322435032c0f7bf68bbfd
Khush Jain <khushjain8419@gmail.com>
2026-06-24 22:32:57 +0530

    new

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCcnwWCrx74Z_HSZaO5d3r8nkuzqX0Y8HU",
+  authDomain: "liveauction-5f69a.firebaseapp.com",
+  projectId: "liveauction-5f69a",
+  storageBucket: "liveauction-5f69a.firebasestorage.app",
+  messagingSenderId: "1090117671517",
+  appId: "1:1090117671517:web:dd0ea582d0e0f810d34bb4",
+  measurementId: "G-YP9TC288YK"
+};
